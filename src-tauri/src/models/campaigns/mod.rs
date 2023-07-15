dbc69f640c4e81e44fa85cbbd00f747aa33c77d3
Solomon Lambert <sol.lambert@gmail.com>
2023-07-15 13:34:46 -0500

    change file structure for campaign models

--- src-tauri/src/models/campaigns/mod.rs
+++ src-tauri/src/models/campaigns/mod.rs
@@ -1,2 +1,13 @@
-pub mod campaigns;
 pub mod campaign_notes;
+
+use diesel::prelude::*;
+
+use crate::schema::campaigns;
+
+#[derive(Queryable, Selectable, Identifiable, PartialEq, Debug, Clone)]
+#[diesel(table_name = campaigns)]
+pub struct Campaigns {
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+}

25303943ccb81cf958d72ccb74d0f1844dd29fc4
Solomon Lambert <sol.lambert@gmail.com>
2023-07-15 13:48:44 -0500

    create model for chatgpt usagess

--- src-tauri/src/models/chatgpt/mod.rs
+++ src-tauri/src/models/chatgpt/mod.rs
@@ -1,2 +1,3 @@
 pub mod chat_gpt_requests;
 pub mod chat_gpt_responses;
+pub mod usages;
---
+++ src-tauri/src/models/chatgpt/usages.rs
@@ -0,0 +1,12 @@
+use diesel::prelude::*;
+
+use crate::schema::usages;
+
+#[derive(Queryable, Selectable, Identifiable, PartialEq, Debug, Clone)]
+#[diesel(table_name = usages)]
+pub struct ChatGPTRequests {
+    pub id: i32,
+    pub prompt_tokens: i32,
+    pub completion_tokens: i32,
+    pub total_tokens: i32,
+}

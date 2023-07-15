550b51188bae4a5880553dade7c0ea92432058b1
Solomon Lambert <sol.lambert@gmail.com>
2023-07-15 02:28:26 -0500

    add logger to store info, refactor lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,29 +1,51 @@
 use diesel::sqlite::SqliteConnection;
 use diesel::prelude::*;
-use std::{error::Error, fs, path::Path, ffi::OsStr};
+use fast_log::Config;
+use log::{info,warn,error};
+use std::{error::Error, fs, path::Path};
 use directories::ProjectDirs;
-
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
 pub fn init() {
     if let Some(proj_dirs) = ProjectDirs::from("com", "sollambert", "dndbuddy") {
         let app_dir = proj_dirs.config_dir();
-        create_folder(app_dir);
-        let db_path = Path::new(app_dir.as_os_str()).join("database.db");
-        if !db_path.exists() {
-            let db_url = format!("file:{}", db_path.to_str().unwrap());
-            let conn = establish_connection(&db_url);
-            match run_migrations(conn) {
-                Ok(()) => {
-                    println!("Migrations successfully completed.")
-                },
-                Err(err) => {
-                    println!("{}", err);
-                }
+        create_logger(app_dir);
+        setup_files(app_dir);
+    }
+}
+
+fn create_logger(path: &Path) {
+    let mut logs_path = path.join("logs");
+    create_folder(&logs_path);
+    logs_path = logs_path.join("dndbuddy.log");
+    match fast_log::init(Config::new()
+        .file(logs_path.as_os_str().to_str().unwrap())) {
+            Ok(_) => {
+                info!("Logger initialized.")
+            },
+            Err(err) => {
+                error!("Logger failed to initialize: {err}")
+            }
+        };
+}
+
+fn setup_files(path: &Path) {
+    create_folder(path);
+    let db_path = path.join("database.db");
+    if !db_path.exists() {
+        let conn = establish_connection(db_path.to_str().unwrap());
+        match run_migrations(conn) {
+            Ok(()) => {
+                info!("Migrations successfully completed.")
+            },
+            Err(err) => {
+                error!("Migrations failed: {err}");
             }
         }
+    } else {
+        info!("Database file found.")
     }
 }
 
@@ -32,10 +54,10 @@ fn create_folder(path: &Path) {
         let dir_builder = fs::DirBuilder::new();
         match dir_builder.create(path) {
             Ok(()) => {
-                println!("Folder created at {:?}", path);
+                info!("Folder created at {:?}", path);
             }, 
             Err(err) => {
-                println!("Error encountered while creating folder: {}", err);
+                error!("Error encountered while creating folder: {err}");
             }
         }
     }

4a6d99ca04a3a237658903beff6cd6ef87163a5a
Solomon Lambert <sol.lambert@gmail.com>
2023-07-15 18:16:30 -0500

    define character enums on frontend as strings for backend compatibility

--- src/lib/character.constants.ts
+++ src/lib/character.constants.ts
@@ -1,25 +1,25 @@
 export enum Race {
-    DWARF,
-    ELF,
-    GNOME,
-    HALF_ORC,
-    HALF_ELF, 
-    HALFLING, 
-    HUMAN, 
-    TIEFLING
+    DWARF = "dwarf",
+    ELF = "elf",
+    GNOME = "gnome",
+    HALF_ORC = "half_orc",
+    HALF_ELF = "half_elf", 
+    HALFLING = "halfling", 
+    HUMAN = "human", 
+    TIEFLING = "tiefling"
 }
 
 export enum Profession {
-    BARBARIAN,
-    BARD, 
-    CLERIC, 
-    DRUID, 
-    FIGHTER, 
-    MONK, 
-    PALADIN, 
-    RANGER, 
-    ROGUE, 
-    SORCEROR, 
-    WARLOCK, 
-    WIZARD
+    BARBARIAN = "barbarian",
+    BARD = "bard", 
+    CLERIC = "cleric", 
+    DRUID = "druid", 
+    FIGHTER = "fighter", 
+    MONK = "monk", 
+    PALADIN = "paladin", 
+    RANGER = "ranger", 
+    ROGUE = "rogue", 
+    SORCEROR = "sorceror", 
+    WARLOCK = "warlock", 
+    WIZARD = "wizard"
 }

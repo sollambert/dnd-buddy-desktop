ac856edc5a8976525277295ce5c2a1921c1d6d33
Solomon Lambert <sol.lambert@gmail.com>
2023-07-15 15:13:39 -0500

    character migration updates for new diesel enum parsing

--- src-tauri/migrations/2023-07-14-225839_create_characters/up.sql
+++ src-tauri/migrations/2023-07-14-225839_create_characters/up.sql
@@ -8,8 +8,8 @@ CREATE TABLE characters (
 	intelligence INTEGER NOT NULL,
 	wisdom INTEGER NOT NULL,
 	charisma INTEGER NOT NULL,
-	race INTEGER NOT NULL,
-	profession INTEGER NOT NULL,
+	race TEXT CHECK(race IN ('Dwarf', 'Elf', 'Gnome', 'HalfOrc', 'HalfElf', 'Halfling', 'Human', 'Tiefling')) NOT NULL,
+	profession TEXT CHECK(profession IN ('Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorceror', 'Warlock', 'Wizard')) NOT NULL,
 	background TEXT,
 	campaign_id INTEGER,
 	FOREIGN KEY(campaign_id) REFERENCES campaigns(id)

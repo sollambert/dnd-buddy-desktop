d43db483a8988559a450d25b9c7ab7a7dd6eb62a
Solomon Lambert <sol.lambert@gmail.com>
2023-07-17 22:58:13 -0500

    newcampaignnote struct and from trait implementation

--- src-tauri/src/models/campaigns/campaign_notes.rs
+++ src-tauri/src/models/campaigns/campaign_notes.rs
@@ -11,3 +11,14 @@ pub struct CampaignNote {
     pub campaign_id: i32,
     pub note: String
 }
+
+pub struct NewCampaignNote {
+    pub campaign_id: i32,
+    pub note: String
+}
+
+impl From<i32> for NewCampaignNote {
+    fn from(campaign_id: i32) -> Self {
+        NewCampaignNote { campaign_id, note: String::new() }
+    }
+}
